8e97673aa426cbfafa280eb62cd50b046ebfb887
Andrey <andreww@ukr.net>
2021-03-01 09:55:49 +0200

    login controller restored, data.sql extended

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -9,6 +9,8 @@ VALUES ('Mathematical', 'Математичний', 3, 5),
        ('Biological', 'Біологічний', 5, 7),
        ('Sociological', 'Соціологічний', 3, 3);
 
+
+
 INSERT INTO user_db (email, password, enabled)
 VALUES ('admin@gmail.com', '$2y$12$OiNQAoK940Y2OfWfA06mWexCgcrToKQgG9RzUX1qjDjXUVs5hXQAG', true),
        ('admin@user.net', '1', true);
@@ -30,6 +32,55 @@ VALUES ('user@ukr.net', '$2y$12$OiNQAoK940Y2OfWfA06mWexCgcrToKQgG9RzUX1qjDjXUVs5
        ('user5@user.net', '$2y$12$OiNQAoK940Y2OfWfA06mWexCgcrToKQgG9RzUX1qjDjXUVs5hXQAG', 'Руслан', 'Черненко',
         'Дмитрович', 'Рівне', 'Рівненська', 'Школа№5', true, 4);
 
+INSERT INTO user_db (email, password, first_name, last_name, patronymic, city, region, school_name, enabled)
+VALUES ('user11@user.net', '$2y$12$OiNQAoK940Y2OfWfA06mWexCgcrToKQgG9RzUX1qjDjXUVs5hXQAG', 'Філіпов', 'Едуард',
+        'Микитович', 'Київ', 'Київська', 'Школа№10', true),
+       ('user12@user.net', '$2y$12$OiNQAoK940Y2OfWfA06mWexCgcrToKQgG9RzUX1qjDjXUVs5hXQAG', 'Іван', 'Харишин',
+        'Сергійович', 'Дуцьк', 'Волинська', 'Школа№4', true),
+       ('user13@user.net', '$2y$12$OiNQAoK940Y2OfWfA06mWexCgcrToKQgG9RzUX1qjDjXUVs5hXQAG', 'Пилип', 'Глібов',
+        'Миколайович', 'Миргород', 'Полтавська', 'Гімназія№7', true),
+       ('user14@user.net', '$2y$12$OiNQAoK940Y2OfWfA06mWexCgcrToKQgG9RzUX1qjDjXUVs5hXQAG', 'Петро', 'Басов',
+        'Дмитрович',
+        'Решитилівка', 'Полтавська', 'Ліцей№5', true),
+       ('user15@user.net', '$2y$12$OiNQAoK940Y2OfWfA06mWexCgcrToKQgG9RzUX1qjDjXUVs5hXQAG', 'Ганна', 'Давидова',
+        'Олексіївна', 'Вінниця', 'Вінницька', 'ПТУ№2', true),
+       ('user16@user.net', '$2y$12$OiNQAoK940Y2OfWfA06mWexCgcrToKQgG9RzUX1qjDjXUVs5hXQAG', 'Іван', 'Швець', 'Дмитрович',
+        'Стрий', 'Львівська', 'Ліцей№1', true),
+       ('user17@user.net', '$2y$12$OiNQAoK940Y2OfWfA06mWexCgcrToKQgG9RzUX1qjDjXUVs5hXQAG', 'Руслан', 'Черненко',
+        'Дмитрович', 'Рівне', 'Рівненська', 'Школа№5', true);
+
+INSERT INTO diploma (math, physics, history, literature, chemistry, biology, user_id)
+VALUES (12, 12, 10, 11, 12, 12, 3),
+       (12, 12, 10, 11, 12, 12, 4),
+       (12, 12, 10, 11, 12, 12, 5),
+       (12, 12, 10, 11, 12, 12, 6),
+       (12, 12, 10, 11, 12, 12, 7),
+       (12, 12, 10, 11, 12, 12, 8),
+       (12, 12, 10, 11, 12, 12, 9),
+       (12, 12, 10, 11, 12, 12, 10),
+       (12, 12, 10, 11, 12, 12, 11),
+       (12, 12, 10, 11, 12, 12, 12),
+       (12, 12, 10, 11, 12, 12, 13),
+       (12, 12, 10, 11, 12, 12, 14),
+       (12, 12, 10, 11, 12, 12, 15),
+       (12, 12, 10, 11, 12, 12, 16);
+
+UPDATE user_db SET diploma_id=1 WHERE id=3;
+UPDATE user_db SET diploma_id=2 WHERE id=4;
+UPDATE user_db SET diploma_id=3 WHERE id=5;
+UPDATE user_db SET diploma_id=4 WHERE id=6;
+UPDATE user_db SET diploma_id=5 WHERE id=7;
+UPDATE user_db SET diploma_id=6 WHERE id=8;
+UPDATE user_db SET diploma_id=7 WHERE id=9;
+UPDATE user_db SET diploma_id=8 WHERE id=10;
+UPDATE user_db SET diploma_id=9 WHERE id=11;
+UPDATE user_db SET diploma_id=10 WHERE id=12;
+UPDATE user_db SET diploma_id=11 WHERE id=13;
+UPDATE user_db SET diploma_id=12 WHERE id=14;
+UPDATE user_db SET diploma_id=13 WHERE id=15;
+UPDATE user_db SET diploma_id=14 WHERE id=16;
+
+
 INSERT INTO user_role (role, user_id)
 VALUES ('ROLE_ADMIN', 1),
        ('ROLE_ADMIN', 2),
@@ -39,7 +90,14 @@ VALUES ('ROLE_ADMIN', 1),
        ('ROLE_USER', 6),
        ('ROLE_USER', 7),
        ('ROLE_USER', 8),
-       ('ROLE_USER', 9);
+       ('ROLE_USER', 9),
+       ('ROLE_USER', 10),
+       ('ROLE_USER', 11),
+       ('ROLE_USER', 12),
+       ('ROLE_USER', 13),
+       ('ROLE_USER', 14),
+       ('ROLE_USER', 15),
+       ('ROLE_USER', 16);
 
 INSERT INTO subject (name_en, name_ua)
 VALUES ('Mathematics', 'Математика'),
@@ -85,3 +143,4 @@ VALUES (3, 5, 100),
        (8, 6, 150),
        (9, 5, 160),
        (9, 6, 160);
+
